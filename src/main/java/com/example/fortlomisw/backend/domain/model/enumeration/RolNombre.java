5f169e730f9c740f73d3390e6478631d184a40e8
Alejandro-Huaman <90795926+Alejandro-Huaman@users.noreply.github.com>
2022-04-19 23:31:13 -0500

    update commit

--- src/main/java/com/example/fortlomisw/backend/domain/model/enumeration/RolNombre.java
+++
@@ -1,9 +0,0 @@
-package com.example.fortlomisw.backend.domain.model.enumeration;
-
-public enum RolNombre {
-
-    Role_Fanatic,Role_Artist
-
-
-
-}
--- src/main/java/com/example/fortlomisw/backend/domain/service/ImageService.java
+++
@@ -1,7 +0,0 @@
-package com.example.fortlomisw.backend.domain.service;
-
-public interface ImageService {
-
-     byte[] compressBytes(byte[] data);
-    byte[] decompressBytes(byte[] data);
-}
